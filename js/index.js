bf2a9ad006aba12fda7d8e46b2c41b9ef365f546
Antony Nyagah <antonynyagah@antonys-MacBook-Pro.local>
2019-02-02 15:12:54 -0500

    commit 2

--- js/index.js
+++ js/index.js
@@ -2,21 +2,55 @@
 
 //variables go here
 
-const 
-const
-const
-const
-const
-const
-const
-const
-const
-const
 
-
-
+const nav = document.querySelectorAll('a');
+const mouseTrack = document.querySelector(".home");
+const headerChange = document.querySelector(".main-navigation");
+const headerChangeAgain = document.querySelector(".main-navigation");
+const textChange = document.querySelector(".home");
+const titleChange = document.querySelector("h1");
+const body  = document.querySelector("body");
 
 
 
 
 //event listeners
+
+// //nav event
+nav.forEach( tags => {
+    tags.addEventListener('click', e => {
+        e.preventDefault();
+    })});
+
+
+//Changes text color to purple once clicked anywhere with non-primary mouse button
+textChange.addEventListener('auxclick', e =>{
+    e.target.style.color = "purple";
+});
+
+//Changes Header color to red once mouse exits the header
+headerChangeAgain.addEventListener('mouseleave', e =>{
+    e.target.style.backgroundColor = "red";
+});
+
+//Changes Header color once mouse enters it
+headerChange.addEventListener('mouseenter', e =>{
+    e.target.style.backgroundColor = "orange";
+});
+
+
+//Changes Fun Bus title to maroon
+titleChange.addEventListener('mouseup', e =>{
+    e.target.style.color = "maroon";
+});
+
+
+//Tells console if mouse is moving
+mouseTrack.addEventListener('mousemove', e =>{
+    console.log(`The mouse is a movin'`);
+});
+
+body.addEventListener('click', e =>{
+	e.target.style.color = 'green';
+	console.log("hello there!");
+});
